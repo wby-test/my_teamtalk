e3ec16f6a0156a3bcdf0ee2a37b43aa3f2744b7f
wby0701 <2829926173@qq.com>
2021-08-22 12:36:43 +0800

    change macro CID_OTHER_VALIDATE_REQ

---
+++ server/src/base/CMakeFiles/base.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wby/src/my_teamtalk/server/src/base/Base64.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/Base64.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/BaseSocket.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/BaseSocket.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/Condition.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/Condition.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/ConfigFileReader.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/ConfigFileReader.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/EncDec.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/EncDec.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/EventDispatch.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/EventDispatch.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/HttpClient.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/HttpClient.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/HttpParserWrapper.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/HttpParserWrapper.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/ImPduBase.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/ImPduBase.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/Lock.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/Lock.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/ServInfo.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/ServInfo.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/Thread.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/Thread.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/ThreadPool.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/ThreadPool.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/TokenValidator.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/TokenValidator.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/UtilPdu.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/UtilPdu.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/http_parser.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/http_parser.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/im_conn_util.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/im_conn_util.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/imconn.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/imconn.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/jsoncpp/json_reader.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_reader.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/jsoncpp/json_value.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_value.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/jsoncpp/json_writer.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/jsoncpp/json_writer.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/log4z.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/log4z.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/netlib.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/netlib.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.BaseDefine.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.BaseDefine.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Buddy.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Buddy.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.File.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.File.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Group.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Group.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Login.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Login.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Message.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Message.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Other.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Other.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.Server.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.Server.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/pb/protocol/IM.SwitchService.pb.cc" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/pb/protocol/IM.SwitchService.pb.cc.o"
+  "/home/wby/src/my_teamtalk/server/src/base/util.cpp" "/home/wby/src/my_teamtalk/server/src/base/CMakeFiles/base.dir/util.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  ".."
+  "./jsoncpp"
+  "./pb"
+  "./pb/protocol"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/AutoPool.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/AutoPool.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CachePool.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/CachePool.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/DBPool.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/DBPool.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/HandlerMap.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/HandlerMap.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/ProxyConn.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/ProxyConn.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/ProxyTask.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/ProxyTask.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/SyncCenter.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/SyncCenter.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/AudioModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/AudioModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/DepartAction.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/DepartAction.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/DepartModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/DepartModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/ExterLogin.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/ExterLogin.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/FileAction.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/FileAction.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/FileModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/FileModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/GroupAction.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/GroupAction.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/GroupMessageModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/GroupMessageModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/GroupModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/GroupModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/InterLogin.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/InterLogin.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/Login.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/Login.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/MessageContent.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/MessageContent.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/MessageCounter.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/MessageCounter.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/MessageModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/MessageModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/RecentSession.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/RecentSession.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/RelationModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/RelationModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/SessionModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/SessionModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/UserAction.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/UserAction.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/business/UserModel.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/business/UserModel.cpp.o"
+  "/home/wby/src/my_teamtalk/server/src/db_proxy_server/db_proxy_server.cpp" "/home/wby/src/my_teamtalk/server/src/db_proxy_server/CMakeFiles/db_proxy_server.dir/db_proxy_server.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "AC_HAS_CRITICAL"
+  "AC_HAS_DEBUG"
+  "AC_HAS_ERROR"
+  "AC_HAS_INFO"
+  "AC_HAS_WARNING"
+  "ENCRYPT"
+  "LINUX_DAEMON"
+  "TIXML_USE_STL"
+  "_FILE_OFFSET_BITS=64"
+  "_REENTRANT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "./businiess"
+  "../base"
+  "../base/jsoncpp"
+  "../base/pb/protocol"
+  "../base/pb"
+  "/usr/include/mysql"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
